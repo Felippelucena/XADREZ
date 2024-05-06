3f628c5d871cc3126aea0563c22a543866f87a0e
Felippelucena <felippelucena.artwork@gmail.com>
2024-05-06 13:25:53 -0300

    ajuste no desenho das ids

--- src/tabuleiro.py
+++ src/tabuleiro.py
@@ -7,12 +7,15 @@ class Tabuleiro:
         self.oi = 'oi'
 
     def desenhar_tabuleiro(self, tela, movimentos):
+        
         FONTE = pygame.font.SysFont('arial', 20, True, False)
         casas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        
+        #Desenhar tabuleiro
         for row in range(8):
             for col in range(8):
                 if (row, col) in movimentos:
-                    cor = (255, 0, 0)
+                    cor = (238, 90, 75)
                     id_cor = (255, 255, 255)
                 elif (row + col) % 2 == 0:
                     cor = (234, 235, 200)
@@ -20,8 +23,17 @@ class Tabuleiro:
                 else:
                     cor = (119, 154, 88)
                     id_cor = (234, 235, 200)
-                pygame.draw.rect(tela, cor, (col * TAMANHO_QUADRADO+100, row * TAMANHO_QUADRADO+100, TAMANHO_QUADRADO, TAMANHO_QUADRADO))                    
-                id_row = FONTE.render(f'{8-row}', True, cor)
-                tela.blit(id_row, (105, 105+(row*100)))
-                id_col = FONTE.render(f'{casas[col]}', True, id_cor)
-                tela.blit(id_col, (185+(col*100), 875))    
+                pygame.draw.rect(tela, cor, (col * TAMANHO_QUADRADO+100, row * TAMANHO_QUADRADO+100, TAMANHO_QUADRADO, TAMANHO_QUADRADO))
+                
+        #Desenhar ids das linhas e colunas
+        for row in range(8):              
+            if (row + col) % 2 == 0:
+                cor = (234, 235, 200)
+                id_cor = (119, 154, 88)
+            else:
+                cor = (119, 154, 88)
+                id_cor = (234, 235, 200)              
+            id_row = FONTE.render(f'{8-row}', True, cor)
+            tela.blit(id_row, (105, 105+(row*100)))
+            id_col = FONTE.render(f'{casas[row]}', True, id_cor)
+            tela.blit(id_col, (185+(row*100), 875))    
